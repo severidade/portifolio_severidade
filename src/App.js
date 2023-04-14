9d35e367d387aad4dec1d5b11791087ae9281da1
severo <oliveira.severo@gmail.com>
2023-04-14 11:16:45 -0300

    feat: :rocket: Cria o componente Navbar
    Também implementa o css reset no app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Reset.css';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 class App extends React.Component {
   render() {
     return (
       <>
+        <Navbar />
         <p>Olá mundo</p>
       </>
     );
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Navbar.module.css';
+
+const Navbar = () => (
+  <div className={ styles.navbar_container }>
+    <p>bola</p>
+  </div>
+);
+
+export default Navbar;
