0931fdd1ba8cf4c9661550dd203ef2f2e001cdc3
severo <oliveira.severo@gmail.com>
2023-04-14 10:35:53 -0300

    refactor: Cria o arquivo App.js
    O padrão do React de criar um arquivo index.js e dentro dele importar o componente App é uma convenção que ajuda a organizar o projeto.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <p>Olá mundo</p>
+      </>
+    );
+  }
+}
+
+export default App;
