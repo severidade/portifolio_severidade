53b1acebddc08019678c54fa21e3d347f01c3a60
severo <oliveira.severo@gmail.com>
2023-04-14 17:45:33 -0300

    refactor: :recycle: Passando props para o caomponente Navbar
    Decisao de organizar o código importando Conteúdo no componente pai.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './Reset.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Content from './data/Content';
 
 export default function App(){
   return (
     <>
-      <Navbar />
+      <Navbar content={ Content } />
     </>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import styles from './Navbar.module.css';
-import Content from '../../data/Content';
+import PropTypes from 'prop-types';
+// import Content from '../../data/Content';
 
-export default function Navbar() {
+export default function Navbar(props) {
+  const { content } = props;
+
   return(
     <div className={ styles.navbar_container }>
         <div className={ styles.contexto_cidade }>BH</div>
         <h1 className={ styles.logotipo }>Marco <span>Severo</span></h1>
         <ul className={ styles.container_social }>
-          {Content.navbar.social_links?.map((item) =>(
+          {content.navbar.social_links?.map((item) =>(
             <li key={item.name} > 
               <a 
                 // className={`${item.name.toLowerCase()}`}
@@ -26,3 +29,7 @@ export default function Navbar() {
     </div>
   )
 }
+
+Navbar.propTypes = {
+  content: PropTypes.object.isRequired,
+};
