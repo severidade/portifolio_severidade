b53a82ccda4021a520d9a1a976d7c23fabd2dc86
severo <oliveira.severo@gmail.com>
2023-07-21 10:52:22 -0300

    feat: :rocket: remove da home seção de projetos
    adiciona a indicação de onde devera entrar o componetne slider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,16 +4,21 @@ import Content from '../data/Content';
 import Navbar from '../components/Navbar';
 import About from '../components/About';
 import Skills from '../components/Skills';
-import Works from '../components/Works';
+// import Works from '../components/Works';
+
+import avatarImage from '../img/works/mini/sistema_solar.png';
 
 export default function Home() {
 
   return(
     <main className='container_main'>
       <Navbar content={ Content } />
+      <div className='slider'>
+        <img src={avatarImage} alt="Avatar" />
+      </div>
       <About content={ Content } />
       <Skills content={ Content } />
-      <Works content={ Works } />
+      {/* <Works content={ Works } /> */}
     </main>
   )
 }
