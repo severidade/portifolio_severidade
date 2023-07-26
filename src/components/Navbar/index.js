8f92a21587f0d4578d0a6d15f9f2b7f994f26ad2
severo <oliveira.severo@gmail.com>
2023-07-26 20:11:18 -0300

    feat: :rocket: inicia implementação de menu

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import styles from './Navbar.module.css';
+import React, { useState }  from 'react';
+// import styles from './Navbar.module.css';
 import PropTypes from 'prop-types';
 // import Content from '../../data/Content';
 
+import './Navbar.css';
+
+
 export default function Navbar(props) {
   const { content } = props;
+  const [menuOpen, setmenuOpen] = useState(false);
+  
+  const toggleMenu = () => {
+    setmenuOpen(!menuOpen);
+  };
 
   return(
-    <div className={ styles.navbar_container }>
-        <div className={ styles.menu}>
-          <div className={ styles.dot }/>
+    <div className="navbar_container">
+        <div
+          // className="menu"
+          className={`menu ${menuOpen ? 'open' : ''}`}
+          onClick={ toggleMenu }
+        >
+          <div className=" dot"/>
         </div>
-        <h1 className={ styles.logotipo }>Marco <span>Severo</span></h1>
-        <ul className={ styles.container_social }>
+        <h1 className="logotipo">Marco <span>Severo</span></h1>
+        <ul className=" container_social">
           {content.navbar.social_links?.map((item) =>(
             <li key={item.name} > 
               <a 
-                // className={`${item.name.toLowerCase()}`}
-                className={`${styles[item.name.toLowerCase()]}`}
+                className={`${item.name.toLowerCase()}`}
+                // className={`${styles[item.name.toLowerCase()]}`}
                 target="_blank"
                 href={ item.href }
                 rel="noreferrer"
@@ -27,7 +39,7 @@ export default function Navbar(props) {
             </li>
           ))}
         </ul>
-        <div className={ styles.contexto_pais }>Brasil</div>
+        <div className="contexto_pais">Brasil</div>
     </div>
   )
 }
