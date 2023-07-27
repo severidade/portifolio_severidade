1314ade3f9c63c06dc65f0c7c8140ea0af2690ac
severo <oliveira.severo@gmail.com>
2023-07-27 17:54:19 -0300

    feat: :rocket: Configura as rotas

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import Error from './pages/Error';
 
 
 export default function App(){
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path='/projects' element= {<Projects />} />
+      <Route path='/contact' element= {<Contact />} />
+      <Route path="*" element={<Error /> } />
     </Routes>
   );
 }
