bb7ae2b76fdf5801c503eb0f18697348f375a568
severo <oliveira.severo@gmail.com>
2023-07-28 20:42:45 -0300

    feat: :rocket: Fecha menu com touch

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,29 +1,42 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import MobileDetect from 'mobile-detect';
-
-
 import './Navbar.css';
-
 
 export default function Navbar(props) {
   const { content } = props;
   const [menuOpen, setMenuOpen] = useState(false);
+  const [touchStartX, setTouchStartX] = useState(null);
 
   const isMobileDevice = () => {
     const md = new MobileDetect(window.navigator.userAgent);
     return md.mobile() !== null;
   };
-  
+
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
   const disableScrollOnWheel = (event) => {
     event.preventDefault();
-  }
-  
+  };
+
+  const handleTouchStart = (event) => {
+    setTouchStartX(event.touches[0].clientX);
+  };
+
+  const handleTouchMove = (event) => {
+    if (menuOpen) {
+      const touchCurrentX = event.touches[0].clientX;
+      const touchDiffX = touchStartX - touchCurrentX;
+
+      // Se o usuário deslizar a tag nav para a esquerda em pelo menos 50 pixels, feche o menu
+      if (touchDiffX > 20) {
+        setMenuOpen(false);
+      }
+    }
+  };
 
   useEffect(() => {
     if (isMobileDevice()) {
@@ -43,46 +56,46 @@ export default function Navbar(props) {
     };
   }, [menuOpen]);
 
-  return(
+  return (
     <>
       <div className="navbar_container">
-          <div
-            // className="menu"
-            className={`menu ${menuOpen ? 'open' : ''}`}
-            onClick={ toggleMenu }
-          >
-            <div className=" dot"/>
-          </div>
-          <h1 className="logotipo">Marco <span>Severo</span></h1>
-          <ul className=" container_social">
-            {content.navbar.social_links?.map((item) =>(
-              <li key={item.name} > 
-                <a 
-                  className={`${item.name.toLowerCase()}`}
-                  // className={`${styles[item.name.toLowerCase()]}`}
-                  target="_blank"
-                  href={ item.href }
-                  rel="noreferrer"
-                >
-                  { item.name }
-                </a>
-              </li>
-            ))}
-          </ul>
-          <div className="contexto_pais">Brasil</div>
+        <div
+          className={`menu ${menuOpen ? 'open' : ''}`}
+          onClick={toggleMenu}
+        >
+          <div className=" dot" />
+        </div>
+        <h1 className="logotipo">Marco <span>Severo</span></h1>
+        <ul className=" container_social">
+          {content.navbar.social_links?.map((item) => (
+            <li key={item.name} >
+              <a
+                className={`${item.name.toLowerCase()}`}
+                target="_blank"
+                href={item.href}
+                rel="noreferrer"
+              >
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="contexto_pais">Brasil</div>
       </div>
       <nav
         className={`menu_options ${menuOpen ? 'open' : ''}`}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
       >
         <button
           className='close_menu'
-          onClick={ toggleMenu }
+          onClick={toggleMenu}
         >
           close
         </button>
         <NavLink to="/" onClick={toggleMenu}>Home</NavLink>
-        <NavLink to="/projects" onClick={ toggleMenu }>Projetos</NavLink>
-        <NavLink to="/contact" onClick={ toggleMenu }>Contato</NavLink>
+        <NavLink to="/projects" onClick={toggleMenu}>Projetos</NavLink>
+        <NavLink to="/contact" onClick={toggleMenu}>Contato</NavLink>
       </nav>
     </>
   )
