d2733033e6f84fdcf9bbc113a833dfd19ff0ae64
severo <oliveira.severo@gmail.com>
2023-09-08 09:18:40 -0300

    style: :art: Cria o CSS Modules

--- src/components/ProjectDetails/index.js
+++ src/components/ProjectDetails/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from'./ProjectDetails.module.css'
 
 export default function ProjectDetails({ currentProject }) {
   const { title, description } = currentProject;
 
   const parse = require('html-react-parser');
   return (
-    <div className='about_project'>
+    <div className={ styles.about_project }>
+      <div className={ styles.image_projeto } ></div> 
       <h1>{title}</h1>
       <div> {parse(description)} </div>
     </div>
