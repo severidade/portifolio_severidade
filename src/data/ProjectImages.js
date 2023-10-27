52d15755ce7b8dc0e08cb25ba520e3f08cb27804
severo <oliveira.severo@gmail.com>
2023-10-27 20:03:44 -0300

    feat: :rocket: Atualiza imagens em wallet

--- src/data/ProjectImages.js
+++ src/data/ProjectImages.js
@@ -13,7 +13,7 @@ const projectImages = {
     ],
   },
   9: {
-    avatar: require('../img/works/wallet/re.png'),
+    avatar: require('../img/works/wallet/wallet_avatar.jpg'),
     gallery: [
       require('../img/works/wallet/bebidas.gif'),
       require('../img/works/wallet/01.png')
