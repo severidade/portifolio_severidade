7df98b734df0c4794c5cea902f0ce1fc6a0ce06f
severo <oliveira.severo@gmail.com>
2023-10-30 17:02:27 -0300

    feat: :rocket: campo e-mail passa por validação regex

--- src/components/ReactEmail/index.js
+++ src/components/ReactEmail/index.js
@@ -1,17 +1,50 @@
 import React, { useState } from 'react';
 import styles from './reactemail.module.css';
-// import PropTypes from 'prop-types';
+import emailjs from '@emailjs/browser';
 
 
 export default function ReactEmail() {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [message, setMessage] = useState('')
-  
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+
+    const templateParams = {
+      from_name: name,
+      message: message,
+      email: email,
+    };
+
+    if ( name === '', email === '', message === ''){
+      alert('Preencha todos os campos');
+      return;
+    }
+
+    if (!email.match(emailRegex)) {
+      alert('Digite um e-mail válido');
+      return;
+    }
+
+    emailjs.send("service_qvpenzq", "template_blfl936", templateParams,"WSfXpb0OvmGtOmgVQ")
+    .then((response) => {
+      console.log("EMAIL ENVIADO: ", response.status, response.text)
+      setName('')
+      setEmail('')
+      setMessage('')
+    }, (err) => {
+      console.log("ERRO: ", err)
+    });
+
+  }
+
   return (
     <div className={ styles.content_form }>
       <h1 className="title">Contato</h1>
-      <form className="form" onSubmit={() => {}}>
+      <form className="form" onSubmit={ sendEmail }>
         <input 
           className="input"
           type="text"
