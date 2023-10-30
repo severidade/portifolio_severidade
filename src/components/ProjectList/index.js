c250cbc57a24689e72f28f5d4ed7db6067be9527
severo <oliveira.severo@gmail.com>
2023-10-30 17:42:32 -0300

    feat: :rocket: Página de projetos listando todos os projetos

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -3,32 +3,37 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styles from './ProjectList.module.css'
 
-export default function ProjectList({ projectList }) {
+export default function ProjectList({ projectList, projectImages }) {
 
   return (
     <main className={ styles.project_list }>
-
-      {projectList.map((project) => (
-        <div
-          className={ styles.project } 
-          key={ project.slug }
-        >
-          <h2> { project.title } </h2>
-          <div className={ styles.image_projeto_mini } >
-            <img src={ project.img } alt={ project.title } />
-          </div> 
-          <Link
-            to={`/projects/${ project.slug }`}
+      {Array.isArray(projectList) && projectList.length > 0 ? (
+        projectList.map((project) => (
+          <div 
+            className={ styles.work }
+            key={project.id}
           >
-            {project.title}
-          </Link>
-        </div>
-      ))}
-
+            <Link to={`/projects/${project.slug}`}>
+              <figure className={ styles.work_avatar }>
+                {projectImages[project.id] && projectImages[project.id].avatar ? (
+                  <img src={projectImages[project.id].avatar} alt={`${project.title} - Avatar`} />
+                ) : (
+                  <p>Sem 📷</p>
+                )}
+              </figure>
+              <h3>{project.title}</h3>
+              <p className={ styles.short_description }>{project.short_description}</p>
+            </Link>
+          </div>
+        ))
+      ) : (
+        <p>Nenhum projeto disponível</p>
+      )}
     </main>
-  );
+);
 }
 
 ProjectList.propTypes = {
   projectList: PropTypes.array.isRequired,
+  projectImages: PropTypes.object.isRequired,
 };
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,16 +3,14 @@ import Content from '../data/Content';
 import Navbar from '../components/Navbar';
 import projectList from '../data/Projects';
 import ProjectList from '../components/ProjectList';
+import projectImages from '../data/ProjectImages';
 
 export default function Projects() {
 
   return(
     <main className='container_project_list'>
       <Navbar content={ Content } />
-      < ProjectList projectList={ projectList }/>
-      {/* <div className='project_list'>
-      
-      </div> */}
+      < ProjectList projectList={ projectList } projectImages={ projectImages } />
     </main>
   )
 }
