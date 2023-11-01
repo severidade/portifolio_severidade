525b4617d231e3f5a80373ca8e11cb644d70b37b
severo <oliveira.severo@gmail.com>
2023-11-01 09:29:38 -0300

    feat: :lipstick: Reduz o tamanho das imagens e elimina as que não estão sendo utilizadas.

--- src/data/ProjectImages.js
+++ src/data/ProjectImages.js
@@ -1,59 +1,59 @@
 const projectImages = {
   1: {
     avatar: require('../img/works/laje/laje_avatar.png'),
-    gallery: [
-      require('../img/works/cicloturismo_agencia/cicloturismo_horizontal.png'),
-    ],
+    // gallery: [
+    //   require('../img/works/cicloturismo_agencia/cicloturismo_horizontal.png'),
+    // ],
   },
   2: {
     avatar: require('../img/works/vereda/avatar_vereda.jpg'),
-    gallery: [
-      require('../img/works/cicloturismo_agencia/cicloturismo_horizontal.png'),
-    ],
+    // gallery: [
+    //   require('../img/works/cicloturismo_agencia/cicloturismo_horizontal.png'),
+    // ],
   },
   3: {
     avatar: require('../img/works/app_receitas/app_receitas_avatar.png'),
-    gallery: [
-      require('../img/works/cicloturismo_agencia/cicloturismo_horizontal.png'),
-    ],
+    // gallery: [
+    //   require('../img/works/cicloturismo_agencia/cicloturismo_horizontal.png'),
+    // ],
   },
   4: {
     avatar: require('../img/works/the_box/box_avatar.png'),
-    gallery: [
-      require('../img/works/cicloturismo_agencia/cicloturismo_horizontal.png'),
-    ],
+    // gallery: [
+    //   require('../img/works/cicloturismo_agencia/cicloturismo_horizontal.png'),
+    // ],
   },
   5: {
     avatar: require('../img/works/sistema_solar/solar_avatar.png'),
-    gallery: [
-      require('../img/works/cicloturismo_agencia/cicloturismo_horizontal.png'),
-    ],
+    // gallery: [
+    //   require('../img/works/cicloturismo_agencia/cicloturismo_horizontal.png'),
+    // ],
   },
   6: {
     avatar: require('../img/works/shopping_cart/shoppin_cart_avatar.png'),
-    gallery: [
-      require('../img/works/cicloturismo_agencia/cicloturismo_horizontal.png'),
-    ],
+    // gallery: [
+    //   require('../img/works/cicloturismo_agencia/cicloturismo_horizontal.png'),
+    // ],
   },
   7: {
     avatar: require('../img/works/cicloturismo_agencia/cicloturismo_avatar.png'),
-    gallery: [
-      require('../img/works/cicloturismo_agencia/cicloturismo_horizontal.png'),
-    ],
+    // gallery: [
+    //   require('../img/works/cicloturismo_agencia/cicloturismo_horizontal.png'),
+    // ],
   },
   8: {
     avatar: require('../img/works/super_trunfo/super_trunfo_avatar.png'),
-    gallery: [
-      require('../img/works/cicloturismo_agencia/sistema_solar.png'),
-      require('../img/works/cicloturismo_agencia/textura.jpg'),
-    ],
+    // gallery: [
+    //   require('../img/works/cicloturismo_agencia/sistema_solar.png'),
+    //   require('../img/works/cicloturismo_agencia/textura.jpg'),
+    // ],
   },
   9: {
     avatar: require('../img/works/wallet/wallet_avatar.png'),
-    gallery: [
-      require('../img/works/wallet/bebidas.gif'),
-      require('../img/works/wallet/01.png')
-    ],
+    // gallery: [
+    //   require('../img/works/wallet/bebidas.gif'),
+    //   require('../img/works/wallet/01.png')
+    // ],
   },
 };
 
